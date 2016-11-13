7d6bb7cae2c2c909cebe6b3a8fd2155ea12456e9
Ilana Solomons <ilanasolomons@gmail.com>
2016-11-13 16:14:53 -0500

    404 error page

--- 404.php
+++ 404.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 
-<div class="main">
+<div class="main_404">
   <div class="container">
 
     <h1>Not Found</h1>
-    <p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
-    <?php get_search_form(); ?>
+    <p>Apologies, but the page you requested could not be found. Please head back to the <a href="http://ilanalily.ca">home page</a>
+    <iframe src="//giphy.com/embed/fKk2I5iiWGN0I" width="480" height="356" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/rip-alan-rickman-he-will-be-missed-fKk2I5iiWGN0I">via GIPHY</a></p>
+    <!-- <?php get_search_form(); ?> -->
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
